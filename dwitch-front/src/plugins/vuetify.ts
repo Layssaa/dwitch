43b2d722c218788aa2ccea680608eb1f588dd8e5
Lay <layssa.lima166@gmail.com>
2025-07-27 03:41:25 -0300

    refactor

--- dwitch-front/src/plugins/vuetify.ts
+++ dwitch-front/src/plugins/vuetify.ts
@@ -5,45 +5,8 @@
  */
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
-import { createVuetify, type ThemeDefinition } from 'vuetify';
-
-const light: ThemeDefinition = {
-  dark: false,
-  colors: {
-    background: '#F2F2F2',
-    surface: '#FFFFFF',
-    primary: '#F23545',
-    toggleColor: '#01122A',
-    'primary-darken-1': '#F23545',
-    secondary: '#51A2FF',
-    'secondary-darken-1': '#51A2FF',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
-    title: '#000000',
-    tertiary: '#01122A',
-  },
-};
-
-const dark: ThemeDefinition = {
-  dark: true,
-  colors: {
-    background: '#000B1A',
-    surface: '#01122A',
-    toggleColor: '#01122A',
-    primary: '#F23545',
-    'primary-darken-1': '#F23545',
-    secondary: '#51A2FF',
-    'secondary-darken-1': '#51A2FF',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
-    title: '#ffffff',
-    tertiary: '#ffffff',
-  },
-};
+import { createVuetify } from 'vuetify';
+import * as themesApp from '@/styles/themes';
 
 export default createVuetify({
   defaults: {
@@ -55,9 +18,6 @@ export default createVuetify({
   },
   theme: {
     defaultTheme: 'dark',
-    themes: {
-      light,
-      dark,
-    },
+    themes: themesApp,
   },
 });
---
+++ dwitch-front/src/styles/themes/light.ts
@@ -0,0 +1,20 @@
+import type { ThemeDefinition } from 'vuetify';
+
+export const light: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#F4F2EE',
+    surface: '#ffffff',
+    primary: '#F23545',
+    toggleColor: '#01122A',
+    'primary-darken-1': '#F23545',
+    secondary: '#51A2FF',
+    'secondary-darken-1': '#51A2FF',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+    title: '#000000',
+    tertiary: '#01122A',
+  },
+};
